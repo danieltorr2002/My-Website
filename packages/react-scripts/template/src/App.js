14165a0a8be682fc8f4e57367cff14bb91f24982
dtorres <Danieltorr2002@gmail.com>
2018-11-11 07:59:03 -0500

    fluff commit

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Daniel Torres
+            Daniel Torres is the greatest.
           </a>
         </header>
       </div>
